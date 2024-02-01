9232a7e1a1a28ba502332fc9d342cf71de11ffea
Campbell <campbela12@q44gcqpvl7.chop.edu>
2024-02-01 11:55:53 -0500

    S american clade version of the other script

---
+++ WhatsGNUSAmericanClade/MakeWhatsGNUCaseCtrlSAmerican.R
@@ -0,0 +1,16 @@
+# Amy Campbell
+# Making list of in-group vs. out-group genomes
+# For WhatsGNU analysis of UTD Chile ST105 tree
+# for the s. american clade rather than the chile-specific clade
+library(dplyr)
+library(stringr)
+
+InSamerican = read.csv("Documents/Planet/ST105/UsefulData/InSAmericanClade.txt",header=F)
+All=read.csv("Documents/Planet/ST105/UsefulData/AllWhatsGNUfiles.txt",header=F)
+All$status="control"
+All = All %>% mutate(status=if_else(V1 %in% InSamerican$V1, "case", "control"))
+All = All %>% filter(V1!="GCF_000009645.1_WhatsGNU_report.txt")
+
+All$InClade = if_else(All$status=="case", 1, 0)
+All$OutOfClade = if_else(All$status=="control", 1, 0)
+All$GenomeName = sapply(All$V1, function(x) str_remove_all(x, "_WhatsGNU_report.txt"))
